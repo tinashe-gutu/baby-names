222dbc67ff3e3aec555b8b2cd7f8b04f18840991
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-10 14:52:39 +0000

    sort data in ascending order by name

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -2,6 +2,7 @@ import data from "../babyNamesData.json";
 import "./BabyName.css";
 
 export function BabyName(): JSX.Element {
+  data.sort((a, b) => a.name.localeCompare(b.name));
   const namesList = data.map((el, i) => {
     if (el.sex === "f") {
       return (
