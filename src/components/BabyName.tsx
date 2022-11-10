9b181b29bd9c9576c521eb17f4dd280b598ce5fd
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-10 14:26:43 +0000

    add gender based classNames to each item

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -1,11 +1,21 @@
 import data from "../babyNamesData.json";
 
 export function BabyName(): JSX.Element {
-  return (
-    <ul>
-      {data.map((el, i) => (
-        <li key={el.id}>{el.name}</li>
-      ))}
-    </ul>
-  );
+  const namesList = data.map((el, i) => {
+    if (el.sex === "f") {
+      return (
+        <li key={el.id} className="girl">
+          {el.name}
+        </li>
+      );
+    } else {
+      return (
+        <li key={el.id} className="boy">
+          {el.name}
+        </li>
+      );
+    }
+  });
+  
+  return <ul>{namesList}</ul>;
 }
