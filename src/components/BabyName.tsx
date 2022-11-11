f40d1f929086697f70735263cdd07932e316342f
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-11 01:45:31 +0000

    set state for tracking favourites

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -1,5 +1,5 @@
 import "./BabyName.css";
-
+import {useState} from 'react'
 interface Names {
   id: number;
   name: string;
@@ -10,26 +10,45 @@ interface Props {
 }
 
 export function BabyName({ data }: Props): JSX.Element {
+  const[favouriteNames, setFavouriteNames] = useState<number[]>([])
+
+  function handleClick(e:React.ChangeEvent<HTMLInputElement>, key:number){
+    setFavouriteNames(():number[]=>{
+      if(favouriteNames.indexOf(key)){
+        const firstHalf = favouriteNames.splice(0,favouriteNames.indexOf(key))
+        const secondHalf = favouriteNames.splice(favouriteNames.indexOf(key, favouriteNames.length))
+        return [...firstHalf, ...secondHalf]
+      }else{
+        return [...favouriteNames, key]
+      }
+    })
+  }
+
   data.sort((a, b) => a.name.localeCompare(b.name));
-  const namesList = data.map((el, i) => {
+  const namesList = data.map((el) => {
     if (el.sex === "f") {
       return (
-        <li key={el.id} className="girl">
+        <a key={el.id} className="girl" onClick={hand}>
           {el.name}
-        </li>
+        </a>
       );
     } else {
       return (
-        <li key={el.id} className="boy">
+        <a key={el.id} className="boy">
           {el.name}
-        </li>
+        </a>
       );
     }
   });
 
   return (
-    <div className="names-container">
-      <ul>{namesList}</ul>
-    </div>
+    <>
+      <div>
+        <Favourites babyName={favouriteNames} onClick={handleClick}/>
+      </div>
+      <div className="names-container">
+        <ul>{namesList}</ul>
+      </div>
+    </>
   );
 }
