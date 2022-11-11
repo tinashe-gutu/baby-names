4755188ea0a48093aa789894d51b0f9ba2c2ccb9
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-11-11 14:07:49 +0000

    add handler functions and states for filtering by gender

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -10,9 +10,18 @@ interface Names {
 interface Props {
   data: Names[];
 }
+interface Active{
+  active:string
+}
 
 export function BabyName({ data }: Props): JSX.Element {
   const [favouriteNames, setFavouriteNames] = useState<number[]>([]);
+  const [isActive, setIsActive] = useState<Active>({active:'all'})
+
+  function handleActiveClick(gender:string){
+    setIsActive((currentActive):Active=>({...currentActive, active:gender}))
+  }
+
 
   function handleClick(key: number, isFavourite = false) {
     setFavouriteNames((prev): number[] => {
@@ -30,7 +39,7 @@ export function BabyName({ data }: Props): JSX.Element {
 
   data.sort((a, b) => a.name.localeCompare(b.name));
   const namesList = data.map((el): JSX.Element => {
-    if (el.sex === "f" && !favouriteNames.some((nums) => nums === el.id)) {
+    if (el.sex === "f" && !favouriteNames.some((nums) => nums === el.id) && isActive.active===('girls'||'all')) {
       return (
         <li key={el.id} className="girl">
           <button onClick={() => handleClick(el.id)}>{el.name}</button>
@@ -39,6 +48,7 @@ export function BabyName({ data }: Props): JSX.Element {
     } else if (
       el.sex === "m" &&
       !favouriteNames.some((nums) => nums === el.id)
+      && isActive.active===('boys'||'all')
     ) {
       return (
         <li key={el.id} className="boy">
@@ -53,6 +63,9 @@ export function BabyName({ data }: Props): JSX.Element {
     <>
       <div>
         <Favourites babyNames={favouriteNames} onClick={handleClick} />
+        <Button onClick={()=>handleActiveClick('all') gender='all'} />
+        <Button onClick={()=>handleActiveClick('girls') gender='girls'} />
+        <Button onClick={()=>handleActiveClick('boys') gender='boys'} />
       </div>
       <div className="names-container">
         <ul>{namesList}</ul>
